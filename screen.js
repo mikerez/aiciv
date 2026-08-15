45f58091da995713e45754f4ac3c45d8987bd8d6
Mike Reznikov <mikerez@github.com>
2026-08-15 18:59:18 +0200

    Align terrain supertile rendering

--- screen.js
+++ screen.js
@@ -595,9 +595,9 @@ function drawSceneFrame(loop)
                             var oppositeI = supertileAnchor.i + 1;
                             var oppositeJ = supertileAnchor.j + 1;
                             var supertileX = (ijtox1(supertileAnchor.i, supertileAnchor.j)
-                                + ijtox1(oppositeI, oppositeJ)) / 2;
+                                + ijtox1(oppositeI, oppositeJ)) / 2 - 40*_ratio;
                             var supertileY = (ijtoy1(supertileAnchor.i, supertileAnchor.j)
-                                + ijtoy1(oppositeI, oppositeJ)) / 2;
+                                + ijtoy1(oppositeI, oppositeJ)) / 2 - 20*_ratio;
                             var supertileBrightness = Math.min(
                                 _screen.tileBrightness(supertileAnchor.i, supertileAnchor.j),
                                 _screen.tileBrightness(supertileAnchor.i, oppositeJ),
