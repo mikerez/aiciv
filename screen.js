b3fe40efd94be9e859eec25b37e57d9a3bdd7f52
Mike Reznikov <mikerez@github.com>
2026-08-15 19:49:17 +0200

    Shift terrain supertiles upward

--- screen.js
+++ screen.js
@@ -597,7 +597,7 @@ function drawSceneFrame(loop)
                             var supertileX = (ijtox1(supertileAnchor.i, supertileAnchor.j)
                                 + ijtox1(oppositeI, oppositeJ)) / 2;
                             var supertileY = (ijtoy1(supertileAnchor.i, supertileAnchor.j)
-                                + ijtoy1(oppositeI, oppositeJ)) / 2 + 10*_ratio;
+                                + ijtoy1(oppositeI, oppositeJ)) / 2 - 10*_ratio;
                             var supertileBrightness = Math.min(
                                 _screen.tileBrightness(supertileAnchor.i, supertileAnchor.j),
                                 _screen.tileBrightness(supertileAnchor.i, oppositeJ),
